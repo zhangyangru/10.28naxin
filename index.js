4e159e19768ae4131a3c55b484845e289caf4626
zhangyangru <39459675+zhangyangru@users.noreply.github.com>
2018-11-10 16:51:30 +0800

    Update index.js
    加粗名称

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-            const story ='<strong>“卓越女生计划”</strong>，全称“西安邮电大学ThoughtWorks联合创新实验室”，是ThoughtWorks公司联合西安邮电大学于2015年12月创立，创立至今也已走了三个年头，在学校芸芸实验室中也是唯一一间以前端为首，后台为辅，从前端学习入手，培养成员学习习惯，学习兴趣，学习热情的实干型实验室。'
+            const story ='<strong>“卓越女生计划”</strong>，全称<strong>“西安邮电大学ThoughtWorks联合创新实验室”</strong>，是ThoughtWorks公司联合西安邮电大学于2015年12月创立，创立至今也已走了三个年头，在学校芸芸实验室中也是唯一一间以前端为首，后台为辅，从前端学习入手，培养成员学习习惯，学习兴趣，学习热情的实干型实验室。'
             const frame = document.getElementById('frame');
             let i = 0;
             const timer=setInterval(function(){
